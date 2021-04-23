3bd67c7761dcb7235237409085dcd08882ad599a
Deqing Wang <33232869+wangdeqing@users.noreply.github.com>
2021-04-23 16:20:10 +0800

    Update InstallToolboxes.m

--- InstallToolboxes.m
+++ InstallToolboxes.m
@@ -1,14 +1,18 @@
 %%
 
 %% Install Tensor Toolbox
+fprintf('Installing Tensor Toolbox ...... \n');
 tensor_toolbox_path = [pwd filesep 'tensor_toolbox-v3.2.1'];
 unzip('tensor_toolbox-v3.2.1.zip',tensor_toolbox_path);
 addpath(genpath(tensor_toolbox_path));
+fprintf('Tensor Toolbox has been installed!\n\n');
 
 %% Install MIR Toolbox 1.6.1
+fprintf('Installing MIR Toolbox 1.6.1 ...... \n');
 mir_toolbox_path = [pwd filesep 'MIRtoolbox1.6.1'];
 unzip('MIRtoolbox1.6.1.zip',mir_toolbox_path);
 addpath(genpath(mir_toolbox_path));
+fprintf('MIR Toolbox 1.6.1 has been installed!\n\n');
 
 %% Install EEGLAB
 % eeglab_path = [pwd filesep 'eeglab2021.0'];
@@ -30,4 +34,4 @@ if ~exist(EEGLAB_path,'dir')
 end
 
 %%
-fprintf('All toolboxes are installed!\n');
+fprintf('All toolboxes have been installed!\n');
